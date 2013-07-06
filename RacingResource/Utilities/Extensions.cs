ab3d69225f0797aacebc0c7563c0e7d33ec2faec
philwith <philip.withington@gmail.com>
2013-07-06 23:06:33 +0100

    Improved address formatting for geocoding

---
+++ RacingResource/Utilities/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using RacingResource.Models;
+
+namespace RacingResource.Utilities
+{
+    public static class Extensions
+    {
+        public static string ToFormattedString(this Address address)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(address.StreetAddress))
+            {
+                sb.Append(address.StreetAddress);
+                sb.Append(", ");
+            }
+            if (!string.IsNullOrEmpty(address.AddressLocality))
+            {
+                sb.Append(address.AddressLocality);
+                sb.Append(", ");
+            }
+            if (!string.IsNullOrEmpty(address.AddressRegion))
+            {
+                sb.Append(address.AddressRegion);
+                sb.Append(", ");
+            }
+            sb.Append(address.PostalCode);
+            return sb.ToString();
+        }
+    }
+
+}
